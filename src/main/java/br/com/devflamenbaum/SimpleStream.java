e7dc385bfbff06b6c9541a806115ca696471dfbf
devflamenbaum <bielflam.engpet@gmail.com>
2024-02-20 11:00:18 -0300

    checking graph utils methods

--- src/main/java/br/com/devflamenbaum/SimpleStream.java
+++ src/main/java/br/com/devflamenbaum/SimpleStream.java
@@ -4,11 +4,13 @@ import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.stream.ThrottleMode;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
@@ -26,13 +28,18 @@ public class SimpleStream {
         Source<String, NotUsed> namesSource = Source.from(names);
         Source<Double, NotUsed> sourcePi = Source.repeat(Math.PI);
         Source<String, NotUsed> repeatingNames = Source.cycle(names::iterator);
-        Source<Integer, NotUsed> infiniteSource = Source.fromIterator(() -> infiniteRange);
+//        Source<Integer, NotUsed> infiniteSource = Source.fromIterator(() -> infiniteRange);
+        Source<Integer, NotUsed> infiniteSource = Source
+                .fromIterator(() -> infiniteRange)
+                .throttle(1, Duration.ofSeconds(3))
+                .take(5);
 
         Flow<Integer, String, NotUsed> flow = Flow.of(Integer.class).map(value -> "The next values is " + value);
         Flow<String, String, NotUsed> nameFlow = Flow.of(String.class).map(value -> "The next name is " + value);
         Flow<Double, String, NotUsed> piFlow = Flow.of(Double.class).map(value -> "The next pi is " + value);
 
         Sink<String, CompletionStage<Done>> sink = Sink.foreach(System.out::println);
+        Sink<String, CompletionStage<Done>> sink2 = Sink.ignore();
 
         RunnableGraph<NotUsed> graph = source.via(flow).to(sink);
         RunnableGraph<NotUsed> graph2 = namesSource.via(nameFlow).to(sink);
@@ -46,7 +53,11 @@ public class SimpleStream {
 //        graph2.run(actorSystem);
 //        graph3.run(actorSystem);
 //        graph4.run(actorSystem);
-        graph5.run(actorSystem);
+//        graph5.run(actorSystem);
 
+//        sink.runWith(repeatingNames.via(nameFlow), actorSystem);
+//        equivalent to: repeatingNames.via(nameFlow).to(sink).run(actorSystem);
+
+        repeatingNames.via(nameFlow).runForeach(System.out::println, actorSystem);
     }
 }

48db5bbeff8484d7a95366517f432d5277eee1dc
devflamenbaum <bielflam.engpet@gmail.com>
2024-02-22 15:33:16 -0300

    use grouped Flow

--- src/main/java/br/com/devflamenbaum/ExploringFlows.java
+++ src/main/java/br/com/devflamenbaum/ExploringFlows.java
@@ -17,11 +17,14 @@ public class ExploringFlows {
         ActorSystem actorSystem = ActorSystem.create(Behaviors.empty(), "actorSystem");
 
         Source<Integer, NotUsed> numbers = Source.range(1, 200);
+
         Flow<Integer, Integer, NotUsed> flow = Flow.of(Integer.class).filter(value -> value % 17 == 0);
         Flow<Integer, Integer, NotUsed> mapConcatFlow = Flow.of(Integer.class).mapConcat(value -> List.of(value, value + 1, value + 2));
-        Sink<Integer, CompletionStage<Done>> printSink = Sink.foreach(System.out::println);
+        Flow<Integer, List<Integer>, NotUsed> groupedFlow = Flow.of(Integer.class).grouped(3);
 
-        numbers.via(flow).via(mapConcatFlow).to(printSink).run(actorSystem);
+        Sink<List<Integer>, CompletionStage<Done>> printSink = Sink.foreach(System.out::println);
+
+        numbers.via(flow).via(mapConcatFlow).via(groupedFlow).to(printSink).run(actorSystem);
 
 
 
